ffe3e575901fc43e91b17e021116ed58ebe10f48
Maximilien Colange <maximilien@epigenelabs.com>
2023-01-04 11:20:07 +0100

    documentation infrastructure

---
+++ docs/source/conf.py
@@ -0,0 +1,54 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import inmoose
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'InMoose'
+copyright = '2022-2023, Maximilien Colange'
+author = 'Maximilien Colange'
+
+version = inmoose.__version__
+release = version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# logo
+html_log = "epigenelogo.png"
+html_favicon = "epigenelogo_favicon.png"
+
+import sphinx_rtd_theme
+extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx_rtd_theme',
+]
+
+# Napoleon settings
+# https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+
+# Turn on autosummary
+autosummary_generate = True
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# TODO temporary work-around
+autodoc_mock_imports = ["edgepy_cpp"]
+
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+Welcome to InMoose documentation!
+=================================
+
+InMoose is the INtegrated Multi Omic Open Source Environment.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. autosummary::
+   :toctree: _autosummary
+   :template:
+   :recursive:
+
+   inmoose
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
