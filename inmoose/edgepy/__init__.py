544ff73c81cc9d78479a3c068aae31d51c30257e
Maximilien Colange <maximilien@epigenelabs.com>
2023-05-16 10:22:21 +0200

    minor code improvements
    - better imports in module edgepy
    - remove a superfluous function
    - code cleaning

--- inmoose/edgepy/__init__.py
+++ inmoose/edgepy/__init__.py
@@ -4,6 +4,7 @@ from .aveLogCPM import aveLogCPM
 from .DGEGLM import DGEGLM
 from .DGEList import DGEList
 from .dispCoxReid import dispCoxReid
+from .dispCoxReidInterpolateTagwise import dispCoxReidInterpolateTagwise
 from .estimateGLMCommonDisp import estimateGLMCommonDisp
 from .estimateGLMTagwiseDisp import estimateGLMTagwiseDisp
 from .glmFit import glmFit
@@ -13,6 +14,8 @@ from .mglmOneGroup import mglmOneGroup
 from .mglmOneWay import mglmOneWay, designAsFactor
 from .movingAverageByCol import movingAverageByCol
 from .nbinomDeviance import nbinomDeviance
+from .predFC import predFC
+from .splitIntoGroups import splitIntoGroups
 from .stats import pnbinom, qnbinom
 from .systematicSubset import systematicSubset
 from .validDGEList import validDGEList
