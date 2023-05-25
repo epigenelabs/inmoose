62862fe448fd7276b7ae5df4755ab00b8f6da6b8
Maximilien Colange <maximilien@epigenelabs.com>
2023-05-25 14:10:21 +0200

    pythonize/cythonize Cox-Reid APL computation

--- inmoose/edgepy/edgepy_cpp/edgepy_cpp.pyx
+++ inmoose/edgepy/edgepy_cpp/edgepy_cpp.pyx
@@ -21,15 +21,21 @@
 # the Bioconductor edgeR package (version 3.38.4):
 # - 'src/nbdev.cpp' and 'src/R_compute_nbdev.cpp' (function
 #   `compute_unit_nb_deviance`)
+# - 'src/R_compute_apl.cpp' and 'src/adj_coxreid.cpp' (functions `compute_apl`
+#   and `acr_compute`)
 # - 'R/q2qnbinom.R' (function _q2qnbinom)
 
 import numpy as np
 cimport cython
-from libcpp.cmath cimport log
+from libcpp.cmath cimport abs, log, isfinite
+
 from scipy.linalg.cython_lapack cimport dormqr, dgeqrf, dtrtrs, dpotrf, dpotrs, dsytrf
 from scipy.linalg.cython_blas cimport dgemv
 from libc.math cimport sqrt
 from scipy.special cimport cython_special as sp
+from scipy.special.cython_special cimport gammaln as lgamma
+from scipy.linalg.lapack import get_lapack_funcs
+
 
 cdef public object make_levenberg_result "make_levenberg_result"(
         ndarray[double, ndim=2] coefficients,
@@ -90,7 +96,16 @@ cdef public void f77_dsytrf "f77_dsytrf"(const char *uplo, const int *n, double 
     dsytrf(<char*>uplo, <int*>n, a, <int*>lda, ipiv, work, <int*>lwork, info)
 
 
+ctypedef fused count_type:
+    long
+    double
+
+
+cdef double low_value = 1e-10
+cdef double log_low_value = log(low_value)
 cdef double mildly_low_value = 1e-8
+
+
 @cython.ufunc
 @cython.cdivision(True)
 cdef double compute_unit_nb_deviance(double y, double mu, double phi):
@@ -193,3 +208,182 @@ cdef double _q2qnbinom(double x, double input_mean, double output_mean, double d
         q2 = sp.gammaincinv(output_mean / ro, p2) * ro
 
     return (q1+q2)/2
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cpdef ndarray compute_apl(const count_type[:,:] y, const double[:,:] means, const double[:,:] disps, const double[:,:] weights, bool adjust, ndarray design):
+    """
+    Compute adjusted profile log-likelihoods of genewise negative binomial GLMs
+
+    This is the low-level function called by :func:`adjustedProfileLik`.
+
+    Arguments
+    ---------
+    y : array_like
+        matrix of counts
+    means : array_like
+        matrix of expected means, same shape as :code:`y`
+    disps : array_like
+        matrix of dispersions
+    weights : array_like
+        matrix of observation weights, same shape as :code:`y`
+    adjust : bool
+        whether to use Cox-Reid adjustment
+    design : array_like
+        the design matrix
+
+    Returns
+    -------
+    ndarray
+        the genewise APL (one element per row in :code:`y`)
+    """
+
+    cdef Py_ssize_t tag, lib
+    cdef double curmu, cury, curd, r, logmur, adj
+    cdef long lwork = -1
+
+    if adjust:
+        routine = get_lapack_funcs('sytrf_lwork', dtype=np.double)
+        ret = routine(design.shape[1])
+        assert len(ret) == 2
+        if ret[1] != 0:
+            raise ValueError("Internal work array size computation failed: "
+                             "%d" % (ret[1],))
+        lwork = int(ret[0]+0.5)
+        if lwork < 1:
+            lwork = 1
+
+    res = np.zeros(y.shape[0])
+    cdef double[:] sum_loglike = res
+
+    working_weights_array = np.zeros(y.shape[1])
+    cdef double[:] working_weights = working_weights_array
+
+    for tag in range(y.shape[0]):
+        for lib in range(y.shape[1]):
+            # mean should only be zero if count is zero, where the
+            # log-likelihood would then be 0.
+            if means[tag,lib] == 0:
+                continue
+
+            # each y is assumed to be the average of 'weights' counts, so we
+            # convert from averages to the "original sums" in order to compute
+            # NB probabilities
+            curmu = means[tag,lib] * weights[tag,lib]
+            cury = y[tag,lib] * weights[tag,lib]
+            curd = disps[tag,lib] / weights[tag,lib]
+
+            # compute the log-likelihood
+            r = 1 / curd
+            logmur = log(curmu + r)
+
+            if curd > 0:
+                sum_loglike[tag] += cury*log(curmu) - cury*logmur + r*log(r) - r*logmur + lgamma(cury+r) - lgamma(cury+1) - lgamma(r)
+            else:
+                sum_loglike[tag] += cury*log(curmu) - curmu - lgamma(cury+1)
+
+            # adding the Jacobian, to account for the fact that we actually
+            # want the log-likelihood of the _scaled_ NB distribution (after
+            # dividing the original sum by the weight).
+            sum_loglike[tag] += log(weights[tag,lib])
+
+            if adjust:
+                # computing W, the matrix of NB working weights
+                # this is used to compute the Cox-Reid adjustment factor
+                working_weights[lib] = curmu / (1 + curd*curmu)
+
+        if adjust:
+            if design.shape[1] == 1:
+                adj = working_weights_array.sum()
+                adj = 0.5*log(abs(adj))
+                sum_loglike[tag] -= adj
+            else:
+                sum_loglike[tag] -= acr_compute(working_weights, design, lwork)
+
+    return res
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cdef double acr_compute(const double[:] wptr, ndarray design, long lwork):
+    """
+    Compute the Cox-Reid adjustment factor
+
+    XtWX represents the expected Fisher information. The overall strategy is to
+    compute the log-determinant of this matrix, to compute the adjustment factor
+    for the likelihood (in order to account for uncertainty in the nuisance
+    parameters i.e. the fitted values).
+
+    We want to apply the Cholesky decomposition to the XtWX matrix. However, to
+    be safe, we call the routine to do a symmetric indefinite factorization i.e.
+    A = LDLt. This guarantees factorization for singular matrices when the
+    actual Cholesky decomposition would fail because it would start whining
+    about non-positive eigenvectors.
+
+    We then try to compute the determinant of XtWX, using two facts:
+
+    - for triangular matrices, the determinant is the product of the diagonals
+    - det(LDL*) = det(L)*det(D)*det(L*)
+    - all diagonal elements of L are unity
+
+    Thus, all we need to do is to sum over all log'd diagonal elements in D. We
+    then divide by two, because that is just the definition of the Cox-Reid
+    adjustment.
+
+    If one of the diagonal elements is zero or NA, we replace it with an
+    appropriately small non-zero value. This is valid because the zero elements
+    correspond to all-zero column in WX, which in turn only arises when there
+    are fitted values of zero, which will be constant at all dispersions. Thus,
+    any replacement value will eventually cancel out during interpolation to
+    obtain the CRAPLE.
+
+    Note that the scipy routine will also do some pivoting, essentially solving
+    PAP* = LDL* for some permutation matrix P. This should not affect anything:
+    the determinant of the permutation matrix is either 1 or -1, but it cancels
+    out, so det(A) = det(PAP*).
+
+    Further note that the routine can theoretically give block diagonals, but
+    this should not occur for positive (semi)definite matrices, which is what
+    XtWX should always be.
+
+    Arguments
+    ---------
+    wptr : array_like
+        weights array
+    design : array_like
+        the design matrix
+
+    Returns
+    -------
+    float
+        the Cox-Reid adjustment factor
+    """
+    cdef double res = 0
+    cdef Py_ssize_t i
+
+    xtwx = (design.T * wptr) @ design
+
+    # BEGIN LDL* decomposition
+    solver = get_lapack_funcs('sytrf', dtype=np.double)
+    ldu, _, info = solver(xtwx, lwork=lwork, lower=True, overwrite_a=False)
+    if info < 0:
+        raise ValueError('{} exited with the internal error "illegal value '
+                         'in argument number {}". See LAPACK documentation '
+                         'for the error codes.'.format('DSYTRF', -info))
+    # END LDL* decomposition
+
+    cdef double[:,:] d = ldu
+
+    # log-determinant as sum of the log-diagonals, then halving
+    assert d.shape[0] == d.shape[1]
+    for i in range(ldu.shape[0]):
+        if d[i,i] < low_value or not isfinite(d[i,i]):
+            res += log_low_value
+        else:
+            res += log(d[i,i])
+
+    return 0.5*res
+
