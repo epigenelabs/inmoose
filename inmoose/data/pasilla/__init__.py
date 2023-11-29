bacda675412682e4e3f77de709911e062789bc0f
Maximilien Colange <maximilien@epigenelabs.com>
2023-11-29 10:15:30 +0100

    add a function to load the pasilla dataset as an anndata

---
+++ inmoose/data/pasilla/__init__.py
@@ -0,0 +1,32 @@
+def pasilla():
+    """
+    Retrieve the pasilla dataset as an :class:`AnnData` object
+
+    The pasilla dataset is an RNA-Seq experiment on the effect of RNAi
+    knockdown of Pasilla, the Drosophila melanogaster ortholog of mammalian
+    NOVA1 and NOVA2, on the transcriptome [Brooks2011]_.
+    """
+    import importlib.resources
+    import pandas as pd
+    import anndata as ad
+
+    data_dir = importlib.resources.files("inmoose.data.pasilla")
+    cts = pd.read_csv(
+        data_dir.joinpath("pasilla_gene_counts.tsv"), sep="\t", index_col=0
+    )
+    anno = pd.read_csv(data_dir.joinpath("pasilla_sample_annotation.csv"), index_col=0)
+
+    # The columns of `cts` and the rows of `anno` use different labels and are
+    # not in the same order. We first need to harmonize them before building the
+    # AnnData object.
+
+    # first get rid of the "fb" suffix
+    anno.index = [i[:-2] for i in anno.index]
+
+    # second reorder the index
+    anno = anno.reindex(cts.columns)
+
+    # we are now ready to build the AnnData object
+    adata = ad.AnnData(cts.T, anno)
+
+    return adata
--- tests/data/test_pasilla.py
+++ tests/data/test_pasilla.py
@@ -1,35 +1,14 @@
 import unittest
 
+from inmoose.data.pasilla import pasilla
+
 
 class Test(unittest.TestCase):
     def test_pasilla(self):
         """
         check that the pasilla dataset can be properly loaded
         """
-        import importlib.resources
-        import pandas as pd
-        import anndata as ad
-
-        data_dir = importlib.resources.files("inmoose.data.pasilla")
-        cts = pd.read_csv(
-            data_dir.joinpath("pasilla_gene_counts.tsv"), sep="\t", index_col=0
-        )
-        anno = pd.read_csv(
-            data_dir.joinpath("pasilla_sample_annotation.csv"), index_col=0
-        )
-
-        # The columns of `cts` and the rows of `anno` use different labels and are
-        # not in the same order. We first need to harmonize them before building the
-        # AnnData object.
-
-        # first get rid of the "fb" suffix
-        anno.index = [i[:-2] for i in anno.index]
-
-        # second reorder the index
-        anno = anno.reindex(cts.columns)
-
-        # we are now ready to build the AnnData object
-        adata = ad.AnnData(cts.T, anno)
+        adata = pasilla()
 
         self.assertEqual(adata.shape, (7, 14599))
         self.assertEqual(len(adata.obs.columns), 5)
