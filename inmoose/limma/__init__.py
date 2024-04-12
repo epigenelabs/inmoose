e131a6e69bf878e86c6a7ef113101fc94032acb6
Maximilien Colange <maximilien@epigenelabs.com>
2024-04-12 17:11:59 +0200

    eBayes function from limma

--- inmoose/limma/__init__.py
+++ inmoose/limma/__init__.py
@@ -1,7 +1,7 @@
 from .contrasts import contrasts_fit, makeContrasts
 from .decidetests import classifyTestsF, TestResults
 from .dups import unwrapdups, uniquegenelist
-from .ebayes import tmixture_matrix, tmixture_vector
+from .ebayes import tmixture_matrix, tmixture_vector, eBayes
 from .fitFDist import fitFDist
 from .lmfit import lmFit, nonEstimable, lm_series
 from .marraylm import MArrayLM
