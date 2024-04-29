78861365a151ce1087e2346b50b1843acaf4100c
Maximilien Colange <maximilien@epigenelabs.com>
2024-04-29 10:34:38 +0200

    documentation improvements

--- inmoose/utils/lm.py
+++ inmoose/utils/lm.py
@@ -21,43 +21,24 @@ from statsmodels.regression.linear_model import WLS
 
 class lm_wfit:
     """
-    Python equivalent to R lm.wfit
+    Python equivalent to R :code:`lm.wfit`
 
     This class is merely a wrapper around :class:`RegressionResults`, the
     return type of :meth:`OLS.fit` and :meth:`WLS.fit`.
 
-    It contains the solution to the equation A X = B, where A is a known
-    matrix, B a known vector and X the unknown to solve for. The solution is
-    obtained by a weighted least-square-error fit based on the QR-decomposition of A.
+    It contains the solution to the equation :math:`A X = B`, where :math:`A`
+    is a known matrix, :math:`B` a known vector and :math:`X` the unknown to
+    solve for.  The solution is obtained by a weighted least-square-error fit
+    based on the QR-decomposition of :math:`A`.
 
     Arguments
     ---------
     design : array_like
-        matrix A of the equation
+        matrix :math:`A` of the equation
     b : array_like
-        offset (vector B in the equation)
+        offset (vector :math:`B` in the equation)
     w : array_like
         weights to use in the least-square-error fit
-
-    Attributes
-    ----------
-    coefficients : ndarray
-        the value found for X
-    residuals : ndarray
-        the residuals, i.e. the difference between A X and B
-    fitted_values : ndarray
-        the value A X
-    effects : ndarray
-        vector of orthogonal single-df effects. The first :code:`rank` of them
-        correspond to the :code:`coefficients`
-    weights : ndarray
-        the weights used in the least-square-error fit
-    rank : int
-        the column rank of matrix A
-    df_residuals : ndarray
-        degrees of freedom of residuals
-    qr : ndarray, ndarray
-        the QR decomposition of matrix A
     """
 
     def __init__(self, design, b, w):
@@ -66,18 +47,31 @@ class lm_wfit:
 
     @property
     def coefficients(self):
+        """
+        ndarray: the value found for :math:`X`
+        """
         return self.fit.params
 
     @property
     def residuals(self):
+        """
+        ndarray: the residuals, *i.e.* the difference between :math:`A X` and :math:`B`
+        """
         return self.fit.resid
 
     @property
     def fitted_values(self):
+        """
+        ndarray: the value :math:`A X`
+        """
         return self.fit.fittedvalues
 
     @property
     def effects(self):
+        """
+        ndarray: vector of orthogonal single-df effects. The first :code:`rank` of them
+        correspond to the :code:`coefficients`
+        """
         if self._effects is None:
             Q, _ = np.linalg.qr(self.fit.model.wexog, mode="complete")
             self._effects = Q.T @ self.fit.model.wendog
@@ -85,56 +79,51 @@ class lm_wfit:
 
     @property
     def weights(self):
+        """
+        ndarray: the weights used in the least-square-error fit
+        """
         return self.fit.weights
 
     @property
     def rank(self):
+        """
+        int: the column rank of matrix :math:`A`
+        """
         return self.fit.model.rank
 
     @property
     def df_residuals(self):
+        """
+        ndarray: degrees of freedom of residuals
+        """
         return self.fit.model.df_resid
 
     @property
     def qr(self):
+        """
+        ndarray, ndarray: the QR decomposition of matrix :math:`A`
+        """
         return self.fit.model.exog_Q, self.fit.model.exog_R
 
 
 class lm_fit(lm_wfit):
     """
-    Python equivalent to R lm.fit
+    Python equivalent to R :code:`lm.fit`
 
     This class is merely a wrapper around :class:`RegressionResults`, the
     return type of :meth:`OLS.fit` and :meth:`WLS.fit`.
 
-    It contains the solution to the equation A X = B, where A is a known
-    matrix, B a known vector and X the unknown to solve for. The solution is
-    obtained by a least-square-error fit based on the QR-decomposition of A.
+    It contains the solution to the equation :math:`A X = B`, where :math:`A`
+    is a known matrix, :math:`B` a known vector and :math:`X` the unknown to
+    solve for.  The solution is obtained by a least-square-error fit based on
+    the QR-decomposition of :math:`A`.
 
     Arguments
     ---------
     design : array_like
-        matrix A of the equation
+        matrix :math:`A` of the equation
     b : array_like
-        offset (vector B in the equation)
-
-    Attributes
-    ----------
-    coefficients : ndarray
-        the value found for X
-    residuals : ndarray
-        the residuals, i.e. the difference between A X and B
-    fitted_values : ndarray
-        the value A X
-    effects : ndarray
-        vector of orthogonal single-df effects. The first :code:`rank` of them
-        correspond to the :code:`coefficients`
-    rank : int
-        the column rank of matrix A
-    df_residuals : ndarray
-        degrees of freedom of residuals
-    qr : ndarray, ndarray
-        the QR decomposition of matrix A
+        offset (vector :math:`B` in the equation)
     """
 
     def __init__(self, design, b):
