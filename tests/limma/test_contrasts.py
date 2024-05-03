32bff9f74216f226afd85afe16350b4349c69e3b
Maximilien Colange <maximilien@epigenelabs.com>
2024-05-03 11:44:47 +0200

    fix makeContrasts with design matrices

--- tests/limma/test_contrasts.py
+++ tests/limma/test_contrasts.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+from patsy import dmatrix
 
 from inmoose.limma import makeContrasts
 
@@ -18,5 +19,13 @@ class Test(unittest.TestCase):
             np.array_equal(
                 makeContrasts(contrasts="A-(B+C)/2", levels=["A", "B", "C"]),
                 [[1.0], [-0.5], [-0.5]],
+            )
+        )
+
+        design = dmatrix("0+A+B", {"A": ["a1", "a2", "a1"], "B": ["b1", "b2", "b3"]})
+        self.assertTrue(
+            np.array_equal(
+                makeContrasts(contrasts="A[a1]-A[a2]", levels=design),
+                [[1.0], [-1.0], [0.0], [0.0]],
             )
         )
