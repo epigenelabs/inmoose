e1d661eb8570fbe843068cd9ab1f1a405a88b432
Maximilien Colange <maximilien@epigenelabs.com>
2024-08-30 09:21:22 +0200

    allow arbitrary type for batch and group in sim_rnaseq

--- tests/sim/test_splat.py
+++ tests/sim/test_splat.py
@@ -4,6 +4,9 @@ from inmoose.sim.splat import sim_rnaseq
 
 N = 1000
 M = 10000
+batch = (M // 2) * ["A"] + (M // 2) * ["B"]
+group = (M // 4) * ["A"] + (M // 4) * ["B"]
+group = group + group
 
 
 class Test(unittest.TestCase):
@@ -13,6 +16,8 @@ class Test(unittest.TestCase):
         """
         data = sim_rnaseq(N, M, random_state=42)
         self.assertEqual(data.shape, (N, M))
+        data = sim_rnaseq(N, M, batch=batch)
+        data = sim_rnaseq(N, M, group=group)
 
     def test_sim_scrnaseq(self):
         """
