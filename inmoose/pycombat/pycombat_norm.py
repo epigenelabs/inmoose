a300658668fa36dcbb197b4ae069e2704efc6b66
Lea-Meunier <lea@epigenelabs.com>
2024-09-23 17:23:10 +0200

    fix pycombat typo missclick

--- inmoose/pycombat/pycombat_norm.py
+++ inmoose/pycombat/pycombat_norm.py
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------
-# Copyright (C) 2024 L. Meunier
+# Copyright (C) 2019-2023 A. Behdenna, A. Nordor, J. Haziza, A. Gema and M. Colange
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
