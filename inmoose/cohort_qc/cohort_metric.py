53f9b69b1574d269c03662818f7a6a566648bd97
Lukas Gazdik <lukas@epigenelabs.com>
2025-05-07 16:15:20 +0200

    ✨ Use StandardScaler to fit_transform pca

--- inmoose/cohort_qc/cohort_metric.py
+++ inmoose/cohort_qc/cohort_metric.py
@@ -26,6 +26,7 @@ from scipy import stats
 from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score
 from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler
 from statsmodels.formula.api import ols
 
 from ..utils import round_scientific_notation, truncate_name
@@ -400,12 +401,17 @@ class CohortMetric:
         """
         # Perform PCA for data after batch effect correction
         pca_after = PCA(n_components=self.n_components)
-        pcs_after = pca_after.fit_transform(self.data_expression_df.T)
+        scaler = StandardScaler()
+        pcs_after = pca_after.fit_transform(
+            scaler.fit_transform(self.data_expression_df.T)
+        )
 
         # If data_expression_df_before is not None, compute PCA for it
         if self.data_expression_df_before is not None:
             pca_before = PCA(n_components=self.n_components)
-            pcs_before = pca_before.fit_transform(self.data_expression_df_before.T)
+            pcs_before = pca_before.fit_transform(
+                scaler.fit_transform(self.data_expression_df_before.T)
+            )
             association_matrix_before = self._compute_pc_associations(pcs_before)
         else:
             pca_before = None
